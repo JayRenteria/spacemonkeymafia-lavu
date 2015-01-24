7052280294e14c2afea1637a73887d5c6ce58e10
flogo1982 <florian.goussin@gmail.com>
2015-01-24 10:57:08 -0700

    clock changes

--- index.php
+++ index.php
@@ -10,10 +10,16 @@ include_once("php/misc/header.php");
 	</div>
 	<div class="col-xs-3">
 
-		<h2>clock and table layout CODE: <div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 200px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: transparent;"><a href="" style="text-decoration: none; font-size: 13px; color: rgb(153, 188, 8);"> </a></div><script type="text/javascript" src="http://localtimes.info/clock.php?&cp1_Hex=99bc08&cp2_Hex=f4f4f2&cp3_Hex=2b2b2b&fwdt=200&ham=0&hbg=1&hfg=0&sid=0&mon=0&wek=0&wkf=0&sep=0&widget_number=125"></script></div></h2>
+		<h2>clock and table layout CODE:
+			<div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 200px;">
+				<div id="clock">
+					<a href="" style="text-decoration: none; font-size: 13px; color: rgb(153, 188, 8);"> </a>
+				</div>
+			</div>
+		</h2>
 	</div>
 	<div class="col-xs-5">
-		<h2>list of available tables</h2>
+		<?php include_once "php/forms/availableTimes.php"; ?>
 	</div>
 </div>
 
--- php/misc/footer.php
+++ php/misc/footer.php
@@ -1,5 +1,6 @@
 		</div><!-- end container-fluid -->
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/clock.js"></script>
 		<script src="js/main.js"></script>
 	</body>
 </html>
