3c8a45701df67bd04185300f74c9d8098052d86d
flogo1982 <florian.goussin@gmail.com>
2015-01-24 10:35:05 -0700

    some layout change on the container

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include_once("php/misc/header.php");
 <div class="row-fluid">
 	<div class="col-xs-4">
 		<?php include_once "php/forms/userInfos.php" ?>
-		<h2>user form</h2>
 	</div>
 	<div class="col-xs-3">
 		<h2>clock and table layout</h2>
