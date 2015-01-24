2edf3c552368e8bdebc73c585fb3f4f5c72d8329
flogo1982 <florian.goussin@gmail.com>
2015-01-24 10:37:46 -0700

    list placeholder

--- index.php
+++ index.php
@@ -12,7 +12,14 @@ include_once("php/misc/header.php");
 		<h2>clock and table layout</h2>
 	</div>
 	<div class="col-xs-5">
-		<h2>list of available tables</h2>
+		<h2>Placeholder list:</h2>
+		<ul class="list-group">
+			<li class="list-group-item">Cras justo odio</li>
+			<li class="list-group-item">Dapibus ac facilisis in</li>
+			<li class="list-group-item">Morbi leo risus</li>
+			<li class="list-group-item">Porta ac consectetur ac</li>
+			<li class="list-group-item">Vestibulum at eros</li>
+		</ul>
 	</div>
 </div>
 
