5b5793a0ef8ca7b4bd6ef653e2c069a443ef272d
flogo1982 <florian.goussin@gmail.com>
2015-01-24 11:17:30 -0700

    jquery

--- php/misc/footer.php
+++ php/misc/footer.php
@@ -1,4 +1,5 @@
 		</div><!-- end container-fluid -->
+		<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/clock.js"></script>
 		<script src="js/main.js"></script>
