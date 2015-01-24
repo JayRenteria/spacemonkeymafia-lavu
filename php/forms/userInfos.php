1d854d3035c118b379ee0dc038b3dfbcbd5f4981
flogo1982 <florian.goussin@gmail.com>
2015-01-24 14:02:19 -0700

    test post var + some changes in the form

--- php/forms/userInfos.php
+++ php/forms/userInfos.php
@@ -1,45 +1,57 @@
-<form action="" method="post">
+<form action="" method="post" novalidate>
 
 	<div class="group">
-		<input type="text" name="name"required="">
-		<label>Name:</label>
+		<input type="text" name="name">
+		<label>Name</label>
 	</div>
 	<div class="group">
-		<input type="text" name="email" required>
-		<label>Email:</label>
+		<input type="text" name="email"><br>
+		<label>Email</label>
 	</div>
 	<div class="group">
-		<input type="text" name="phone" required>
-		<label>Phone:</label>
+		<input type="text" name="phone"><br>
+		<label>Phone</label>
 	</div>
-	Date<input type="date" name="date" required>
-	<select name="Time" required>
-		<option value="">Select a Time</option>
-		<option value="7:00AM">7:00AM</option>
-		<option value="7:30AM">7:30AM</option>
-		<option value="8:00AM">8:00AM</option>
-		<option value="8:30AM">8:30AM</option>
-		<option value="9:00AM">9:00AM</option>
-		<option value="9:30AM">9:30AM</option>
-		<option value="10:00AM">10:00AM</option>
-		<option value="10:30AM">10:30AM</option>
-		<option value="11:00AM">11:00AM</option>
-		<option value="11:30AM">11:30AM</option>
-		<option value="12:00PM">12:00PM</option>
-		<option value="12:30PM">12:30PM</option>
-		<option value="1:00PM">1:00PM</option>
-		<option value="1:30PM">1:30PM</option>
-		<option value="2:00PM">2:00PM</option>
-		<option value="2:30PM">2:30PM</option>
-		<option value="3:00PM">3:00PM</option>
-		<option value="3:30PM">3:30PM</option>
-		<option value="4:00PM">4:00PM</option>
-		<option value="4:30PM">4:30PM</option>
-		<option value="5:00PM">5:00PM</option>
-		<option value="5:30PM">5:30PM</option>
-		<option value="6:00PM">6:00PM</option>
-		<option value="6:30PM">6:30PM</option>
-	</select><br>
+	<div class="group">
+		Date<input type="date" name="date"><br>
+	</div>
+	<div class="group">
+		<select name="Time" required>
+			<option value="">Select a Time</option>
+			<option value="7:00AM">7:00AM</option>
+			<option value="7:30AM">7:30AM</option>
+			<option value="8:00AM">8:00AM</option>
+			<option value="8:30AM">8:30AM</option>
+			<option value="9:00AM">9:00AM</option>
+			<option value="9:30AM">9:30AM</option>
+			<option value="10:00AM">10:00AM</option>
+			<option value="10:30AM">10:30AM</option>
+			<option value="11:00AM">11:00AM</option>
+			<option value="11:30AM">11:30AM</option>
+			<option value="12:00PM">12:00PM</option>
+			<option value="12:30PM">12:30PM</option>
+			<option value="1:00PM">1:00PM</option>
+			<option value="1:30PM">1:30PM</option>
+			<option value="2:00PM">2:00PM</option>
+			<option value="2:30PM">2:30PM</option>
+			<option value="3:00PM">3:00PM</option>
+			<option value="3:30PM">3:30PM</option>
+			<option value="4:00PM">4:00PM</option>
+			<option value="4:30PM">4:30PM</option>
+			<option value="5:00PM">5:00PM</option>
+			<option value="5:30PM">5:30PM</option>
+			<option value="6:00PM">6:00PM</option>
+			<option value="6:30PM">6:30PM</option>
+		</select><br>
+	</div>
+
 	<input type="submit" value="Reserve!">
 
 </form>
+
+<!-- Manage the input from the user -->
+<?php
+
+var_dump($_POST);
+
+?>
