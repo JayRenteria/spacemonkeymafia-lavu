9979f65d6f0d9c3d07057a830bb9f0d6c74c569c
flogo1982 <florian.goussin@gmail.com>
2015-01-24 12:24:51 -0700

    clock.php

--- index.php
+++ index.php
@@ -11,26 +11,7 @@ include_once("php/misc/header.php");
 	<div class="col-xs-4">
 
 		<h2>clock and table layout</h2>
-		<div id="clock">
-			<ul>
-				<li class="hour hr1"><span>1</span></li>
-				<li class="hour hr2"><span>2</span></li>
-				<li class="hour hr3"><span>3</span></li>
-				<li class="hour hr4"><span>4</span></li>
-				<li class="hour hr5"><span>5</span></li>
-				<li class="hour hr6"><span>6</span></li>
-				<li class="hour hr7"><span>7</span></li>
-				<li class="hour hr8"><span>8</span></li>
-				<li class="hour hr9"><span>9</span></li>
-				<li class="hour hr10"><span>10</span></li>
-				<li class="hour hr11"><span>11</span></li>
-				<li class="hour hr12"><span>12</span></li>
-			</ul>
-
-			<div id="hour"></div>
-			<div id="minute"></div>
-			<div id="second"></div>
-		</div>
+		<?php include_once "php/misc/clock.php"; ?>
 	</div>
 	<div class="col-xs-4">
 		<?php include_once "php/forms/availableTimes.php"; ?>
---
+++ php/misc/clock.php
@@ -0,0 +1,20 @@
+<div id="clock">
+	<ul>
+		<li class="hour hr1"><span>1</span></li>
+		<li class="hour hr2"><span>2</span></li>
+		<li class="hour hr3"><span>3</span></li>
+		<li class="hour hr4"><span>4</span></li>
+		<li class="hour hr5"><span>5</span></li>
+		<li class="hour hr6"><span>6</span></li>
+		<li class="hour hr7"><span>7</span></li>
+		<li class="hour hr8"><span>8</span></li>
+		<li class="hour hr9"><span>9</span></li>
+		<li class="hour hr10"><span>10</span></li>
+		<li class="hour hr11"><span>11</span></li>
+		<li class="hour hr12"><span>12</span></li>
+	</ul>
+
+	<div id="hour"></div>
+	<div id="minute"></div>
+	<div id="second"></div>
+</div>
