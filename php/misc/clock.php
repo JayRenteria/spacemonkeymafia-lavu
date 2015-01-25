792b51039685611a197534329b746f15f9b6a3bb
flogo1982 <florian.goussin@gmail.com>
2015-01-25 11:49:06 -0700

    test clock

--- php/misc/clock.php
+++ php/misc/clock.php
@@ -1,8 +1,10 @@
 <!--<div class="border-wrapper">-->
 	<div id="clock">
-		<img class="clock-image" src="images/clockInside.png" alt="the clock"/>
-		<div id="hour"></div>
-		<div id="minute"></div>
-		<div id="second"></div>
+		<div class="clock-container">
+			<img class="clock-image" src="images/clockInside.png" alt="the clock"/>
+			<div id="hour"></div>
+			<div id="minute"></div>
+			<div id="second"></div>
+		</div>
 	</div>
 <!--</div><!-- end border-wrapper -->
