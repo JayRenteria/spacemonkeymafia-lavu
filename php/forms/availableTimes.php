0d95e0fe54e2d3e15806f41dad908c7d2de0e031
flogo1982 <florian.goussin@gmail.com>
2015-01-25 04:04:41 -0700

    works on dates subtraction

--- php/forms/availableTimes.php
+++ php/forms/availableTimes.php
@@ -26,10 +26,17 @@ echo "<table border='1' class='table'>
 <th>Time</th>
 <th>Number of Guests</th>
 </tr>";
+//echo date('m-d-Y h:i:s') . "<br>";
+//echo date('m-d-Y h:i:s', date("m-d-Y h:i:s") - 60 * 60 * 2);
 
 foreach($reservations as $reservation) {
 	$newDate = explode(" ", $reservation->getReservationDate()->format('m-d-Y'));
 	$newDate = $newDate[0];
+	$time = $reservation->getReservationTime();
+
+//	if($time < date('h:i:s', date("h:i:s") - 60 * 60 * 2)) {
+//		continue;
+//	}
 	echo "<tr>";
 	echo "<td>" . $reservation->getGuestName() . "</td>";
 	echo "<td>" . $newDate . "</td>";
